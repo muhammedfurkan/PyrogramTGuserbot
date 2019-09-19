53fb978616f86614506e275a8d915b56e6f7261b
Hasibul Kobir <46620128+HasibulKabir@users.noreply.github.com>
2019-09-19 07:31:26 +0600

    Update restart.py

--- pyrouserbot/modules/restart.py
+++ pyrouserbot/modules/restart.py
@@ -2,6 +2,7 @@ import pyrogram
 from pyrogram import *
 from pyrouserbot import cmd
 from time import sleep
+import os, sys
 
 @Client.on_message(Filters.command("restart", cmd) & Filters.me)
 def restart(client, message):
