c40b04a7659d0009f2a088a576801569fe3ba2e2
Hasibul Kobir <46620128+HasibulKabir@users.noreply.github.com>
2019-09-19 07:37:35 +0600

    Update restart.py

--- pyrouserbot/modules/restart.py
+++ pyrouserbot/modules/restart.py
@@ -6,8 +6,8 @@ import os, sys
 
 @Client.on_message(Filters.command("restart", cmd) & Filters.me)
 def restart(client, message):
-    os.execl(sys.executable, sys.executable, *sys.argv)
     message.edit("Bot restarted...")
     sleep(2)
     message.delete()
+    os.execl(sys.executable, sys.executable, *sys.argv)
     quit()
