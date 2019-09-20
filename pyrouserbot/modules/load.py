1163eba400ab8ebf4974280bcb8e893d52e093d5
Hasibul Kobir <46620128+HasibulKabir@users.noreply.github.com>
2019-09-21 02:13:47 +0600

    Update load.py

--- pyrouserbot/modules/load.py
+++ pyrouserbot/modules/load.py
@@ -7,6 +7,11 @@ import pyrouserbot
 async def lo_ad(client, message):
       pname = message.text[6:]
       fpname = "pyrouserbot.modules."+pname
+      path = "/app/pyrouserbot/modules/"+pname+".py"
+      if not os.path.isfile(path):
+            await message.reply(f"There is no module named {pname}")
+            return
+         
       try:
           client.add_handler(fpname)
           await message.reply(f"Successfully loaded {pname}")
