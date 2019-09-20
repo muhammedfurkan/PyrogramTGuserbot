ba6f1e7695ec9948f0da976fedc4a91edace08c1
Hasibul Kobir <46620128+HasibulKabir@users.noreply.github.com>
2019-09-21 02:02:14 +0600

    Update load.py

--- pyrouserbot/modules/load.py
+++ pyrouserbot/modules/load.py
@@ -1,7 +1,7 @@
 import pyrogram
 from pyrogram import *
 from pyrouserbot import app, cmd
-
+import pyrouserbot
 
 @Client.on_message(Filters.command(["load"],cmd) & Filters.me)
 async def lo_ad(client, message):
