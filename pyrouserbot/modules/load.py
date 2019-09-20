8f8a12f843c416972d9794baf687210abd4ab40f
Hasibul Kobir <46620128+HasibulKabir@users.noreply.github.com>
2019-09-21 01:51:55 +0600

    Update load.py

--- pyrouserbot/modules/load.py
+++ pyrouserbot/modules/load.py
@@ -1,12 +1,12 @@
 import pyrogram
 from pyrogram import *
 from pyrouserbot import app, cmd
-import plugins
+
 
 @Client.on_message(Filters.command(["load"],cmd) & Filters.me)
 async def lo_ad(client, message):
       pname = message.command[1:]
-      fpname = plugins+"."+pname
+      fpname = "pyrouserbot.modules."+pname
       try:
           client.add_handler(*fpname)
       except Exception as e:
